f691971b4ca86b4a569990bae90cc7de4e820a23
fluxworks <mrjustinlucas@gmail.com>
2026-03-21 16:04:16 -0400

    26.03.21-1600
    Refactor module handling and argument processing in src/bin.rs.

--- src/bin.rs
+++ src/bin.rs
@@ -10,7 +10,6 @@
 (
 
 )]
-
 /*
 pub mod _
 {
@@ -25,16 +24,26 @@ pub mod _
         *,
     };
 }
-
-
-mklink C:\Users\CKGuest\norman\\mod.exe C:\Users\CKGuest\norman\target\release\mod.exe
 */
+use std::io::BufRead;
+
 pub static mut ARGUMENTS:Vec<String> = vec![];
-pub static mut CURRENT_ARGUMENT:Option<String> = None;
 pub static mut PREVIOUS_ARGUMENT:Option<String> = None;
+pub static mut CURRENT_ARGUMENT:Option<String> = None;
+
+pub static mut MODULES:Vec<String> = vec![];
+pub static mut PREVIOUS_MODULES:Option<String> = None;
+pub static mut CURRENT_MODULES:Option<String> = None;
 
 pub static mut INPUT_FROM:Option<String> = None;
 pub static mut OUTPUT_TO:Option<String> = None;
+
+pub static mut PATHS:Vec<String> = vec![];
+pub static mut PREVIOUS_PATH:Option<String> = None;
+pub static mut CURRENT_PATH:Option<String> = None;
+
+pub static mut LINES:Vec<String> = vec![];
+pub static mut BUFFER:Vec<String> = vec![];
 
 pub mod env
 {
@@ -46,11 +55,125 @@ pub mod error
     pub use std::error::{*};
 }
 
+pub mod fs
+{
+    pub use std::fs::{*};
+}
+
+pub mod io
+{
+    pub use std::io::{*};
+}
+
+pub mod path
+{
+    pub use std::path::{*};
+}
+/*
+create
+read
+edit
+append
+test
+emit
+signal
+*/
+unsafe fn read_module_path( from:&crate::path::Path ) -> Result<(), Box<dyn crate::error::Error>>
+{
+    unsafe
+    {
+        let file = fs::File::open( from ).expect("File not found");
+        let buffer = crate::io::BufReader::new(file);
+
+        for line in buffer.lines()
+        {
+            BUFFER.push( line? )
+        }
+
+        MODULES.push( from.to_string_lossy().into_owned() );
+
+        return Ok(());
+    }
+}
+
+unsafe fn read_module_line( from:&crate::path::Path ) -> Result<(), Box<dyn crate::error::Error>>
+{
+    unsafe
+    {
+        let file = fs::File::open( from ).expect("File not found");
+        let buffer = crate::io::BufReader::new(file);
+        let mut lines:Vec<String> = vec![];
+        //let lined:Vec<String> = BUFFER.clone();
+
+        for line in buffer.lines()
+        {
+            lines.push( line? )
+        }
+
+        lines.append(  &mut BUFFER );
+        BUFFER = lines.clone();
+
+        return Ok(());
+    }
+}
+
+unsafe fn create_module() -> Result<(), Box<dyn crate::error::Error>>
+{
+    unsafe
+    {
+        for line in BUFFER.iter()
+        {
+            //let parts:Vec<&str> = line.split(' ').collect();
+            //println!( r#"parts( {:?} )"#, parts );
+            match true
+            {
+                true if line.starts_with( r#"#[path"# ) =>
+                {
+                    match true
+                    {
+                        true if line.ends_with( r#".rs"]"# ) =>
+                        {
+                            println!("Found Module File Attribute( {} )", line);
+                        }
+
+                        _  =>
+                        {
+                            println!("Found Module Path Attribute( {} )", line);
+                        }
+                    }
+                }
+
+                true if line.starts_with( r#"mod "# ) | line.starts_with( r#"pub mod "# ) =>
+                {
+                    match true
+                    {
+                        true if line.ends_with( r#";"# ) =>
+                        {
+                            println!("Found External Module File Attribute( {} )", line);
+                        }
+
+                        _  =>
+                        {
+                            println!("Found Inline Module( {} )", line);
+                        }
+                    }
+                }
+
+                _  =>
+                {
+
+                }
+            }
+        }
+
+        return Ok(());
+    }
+}
+
 unsafe fn domain() -> Result<(), Box<dyn crate::error::Error>>
 {
     unsafe
     {
-        //OUTPUT_TO = "module.rs".to_string(); 
         let arguments = crate::env::args().skip(1).collect::<Vec<String>>();
 
         match arguments.len()
@@ -90,9 +213,16 @@ usage | mod from <input-file> to <output-file>"#
                     return Ok(());
                 }
 
+                let current_path:String = env::current_dir().unwrap().to_str().unwrap().to_string();
+                PATHS.push(current_path.clone());
+                CURRENT_PATH = Some(current_path.clone());
+                /*
+                println!( r#"paths( {:?} )"#, PATHS );
+                println!( r#"current_path( {:?} )"#, &current_path ); */
+
                 for ( index, argument ) in arguments.iter().enumerate()
                 {
-                    PREVIOUS_ARGUMENT = CURRENT_ARGUMENT;
+                    PREVIOUS_ARGUMENT = CURRENT_ARGUMENT.clone();
                                         
                     let current_argument = argument.clone();
                     
@@ -105,7 +235,7 @@ usage | mod from <input-file> to <output-file>"#
                         
                         current =>
                         {
-                            match PREVIOUS_ARGUMENT
+                            match &PREVIOUS_ARGUMENT
                             {
                                 Some( previous ) =>
                                 {
@@ -113,24 +243,91 @@ usage | mod from <input-file> to <output-file>"#
                                     {
                                         "from" =>
                                         {
-                                            INPUT_FROM = Some(argument.clone());
+                                            if INPUT_FROM.is_none()
+                                            {
+                                                INPUT_FROM = Some(argument.clone());
+                                                println!( r#"INPUT_FROM( {:?} )"#, INPUT_FROM )
+                                            }
+
+                                            else
+                                            {
+                                                println!
+                                                (
+                                                    r#"
+error | input file has already been set.
+Please check the arrangement of the provided arguments.
+
+mod | Replaces in the provided rust file,  every instance of
+(pub) mod module_name with the contents of the corresponding module file.
+
+usage | mod from <input-file> to <output-file>"#
+                                                );
+
+                                                return Ok(());
+                                            }
                                         }
 
                                         "to" =>
                                         {
-                                            OUTPUT_TO = Some(argument.clone());
+                                            if OUTPUT_TO.is_none()
+                                            {
+                                                OUTPUT_TO = Some(argument.clone());
+                                                println!( r#"OUTPUT_TO( {:?} )"#, OUTPUT_TO )
+                                            }
+
+                                            else
+                                            {
+                                                println!
+                                                (
+                                                    r#"
+error | output file has already been set
+Please check the arrangement of the provided arguments.
+
+mod | Replaces in the provided rust file,  every instance of
+(pub) mod module_name with the contents of the corresponding module file.
+
+usage | mod from <input-file> to <output-file>"#
+                                                );
+
+                                                return Ok(());
+                                            }
                                         }
                                         
                                         previously =>
                                         {
-                                            
+                                            println!( r#"previously( {:?} )"#, previously )
                                         }
                                     }
                                 }
                                 
                                 None =>
                                 {
-                                    
+                                    match ARGUMENTS.len()
+                                    {
+                                        0 =>
+                                        {
+                                            INPUT_FROM = Some(argument.clone());
+                                            ARGUMENTS.push( format!( r#"from"# ) );
+                                            println!( r#"INPUT_FROM( {:?} )"#, INPUT_FROM )
+                                        }
+
+                                        _=>
+                                        {
+                                            println!
+                                            (
+                                                r#"
+    error | unexpected arguments
+    Please check the arrangement of the provided arguments.
+
+    mod | Replaces in the provided rust file,  every instance of
+    (pub) mod module_name with the contents of the corresponding module file.
+
+    usage | mod from <input-file> to <output-file>"#
+                                            );
+
+                                            return Ok(());
+                                        }
+                                    }
                                 }
                             }
                         }
@@ -139,7 +336,30 @@ usage | mod from <input-file> to <output-file>"#
                     ARGUMENTS.push(argument.clone());
                 }
 
-                println!( r#"[]]::{:?}"#, ARGUMENTS );
+                println!( r#"ARGUMENTS( {:?} )"#, ARGUMENTS );
+                /*
+                let input_from = INPUT_FROM.clone().unwrap();
+                let cwd = input_from.split( '\\' ).collect::<Vec<&str>>();
+                println!( r#"cwd( {:?} )"#, cwd );
+
+                match CURRENT_PATH == INPUT_FROM.unwrap().s
+                {
+                    true =>
+                    {
+
+                    }
+
+                    _=>
+                    {
+
+                    }
+                } */
+
+                read_module_path(  crate::path::Path::new( &INPUT_FROM.clone().unwrap() ) )?;
+
+                // println!( r#"BUFFER( {:?} )"#, BUFFER );
+
+                create_module()?;
 
                 return Ok(());
             }
